f13f8d293fac398b5e4a0b28aae77be7a6a6c02f
vineet <rvineet02@gmail.com>
2021-10-18 12:54:28 -0400

    Update: - added state variables to keep track of owner and those that call the   functions - simple calling of contract functions by me itself

--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,17 @@
 const main = async () => {
+
+
+	const [owner, randomPerson] = await hre.ethers.getSigners();
+
 	//compile our contract and generate the necessary files we need
 	// under the artifacts directory
 	const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
 	
 	// hardhat will create a local ethereum network for us, but just for this contract. 
 	// evertime you run the contract, it'll create a fresh blockchain to make it easier to debug 
+	// waveContractFactory.deploy() deploys our contract to the blockchain
+	// our functions become available to be called because of the public keyword in declaration (kind of like a public API endpoint)
+
 	const waveContract = await waveContractFactory.deploy();
 	
 	// wait till the contract is officially deployed to our local blockchain;
@@ -13,6 +20,18 @@ const main = async () => {
 
 	//this is how we find our contract on the blockchain. 
 	console.log("Contract Deployed to:", waveContract.address);
+	console.log("Contract deployed by: ", owner.address);
+
+	let waveCount;
+	waveCount = await waveContract.getTotalWaves();
+	const waveTxn = await waveContract.wave();
+	await waveTxn.wait()
+	waveCount = await waveContract.getTotalWaves();
+
+	// lines 25-29 are us manually calling our functions (similar to an API call)
+	// we get # of waves, then wave and get # of waves to check if it has been updated after the wave 
+	
+
 };
 
 const runMain = async() => {
