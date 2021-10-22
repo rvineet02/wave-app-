6c5e3017201a1b1d8098ce057dde97dd41a228fb
vineet <rvineet02@gmail.com>
2021-10-22 12:50:49 -0400

    - Updated Contract to hold spotify links in message - Event to store sender address, timestamp and message on transaction   log - Wave Struct to store these three parameters - emit NewWave event

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -13,17 +13,46 @@ contract WavePortal {
     //it is stored permanantly sotred in contract storage
     uint256 totalWaves;
 
-    constructor() {
-        console.log("yo yo yo, I am a contract and I am smart");
+    // Event:Events are inheritable members of contracts. When you call them, they
+    // cause the arguments to be stored in the transaction’s log — a special data
+    // structure in the blockchain. These logs are associated with the address of the contract
+    // The definition of the event contains the name of the event and
+    // the parameters you want to save when you trigger the event.
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    // struct allows us to create a custom data structure
+    struct Wave {
+        address waver; // address of user who waved
+        string message; // message user sent
+        uint256 timestamp; // timestamp when user waved
     }
 
-    function wave() public {
+    Wave[] waves; // variables waves that is an array of strutcts, Waves
+
+    constructor() {
+        console.log("I am a smart contract");
+    }
+
+    function wave(string memory _message) public {
         totalWaves += 1;
         console.log("%s has waved", msg.sender);
         // msg.sender:
         // wallet address of the person who called the function.
         // essentially like built in auth, keeps track of uniquely who called what function
         // to call a smart contract function, you need to be connected with a valid wallet
+
+        // store data in wavwe array using Wave struct
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        // emiting event so it can be stored in the transaction log
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    // return the struct array
+    // simplifies process to retireve information from our website
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
